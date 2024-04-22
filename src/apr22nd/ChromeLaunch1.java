62bdd5cabe4c04ac8d549cbb292bae6abffbcd3a
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-04-22 21:37:44 +0530

    Selenium Season 1 programs till Apr 22nd

---
+++ src/apr22nd/ChromeLaunch1.java
@@ -0,0 +1,14 @@
+package apr22nd;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch1 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\FLM19thFeb\\jars\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		new ChromeDriver();
+		
+	}
+
+}
